c965cf39756633645e33da0aab669020c404426b
darrenhon <darrenhonsoftware@gmail.com>
2016-03-06 03:08:05 -0800

    Clear unused models in likePredict.r

--- MachineLearning/likePredict.r
+++ MachineLearning/likePredict.r
@@ -1,25 +1,4 @@
-library(e1071)
-library(party)
 library(rPython)
-
-igage = read.csv('ig_age.csv', check.names=F)
-iggender = read.csv('ig_gender.csv', check.names=F)
-tops = scan('top10000.csv')
-
-predictAgeByLikes <- function(userid, dfRelations, model)
-{
-  cols = c(igage[1:100, 1], '31159065182', '20060875764', '167641889920409', '9588466619', '10643211755')
-  df = data.frame(matrix(nrow=1, ncol=length(cols)))
-  colnames(df) = cols
-  for (i in 1:ncol(df)) 
-  {
-    df[,i] = as.factor(df[,i])
-    levels(df[,i]) = c('0','1')
-  }
-  df = rbind(df, as.numeric(cols %in% dfRelations[dfRelations$userid == userid, 'like_id']))
-  df = df[-1,]
-  return(predict(model, df))
-}
 
 initializeKnn <- function(input)
 {
@@ -27,42 +6,19 @@ initializeKnn <- function(input)
   python.exec(sprintf("initialize('%s', '/data/training')", input))
 }
 
-predictGenderByLikes <- function(userid, dfRelations, model)
+predictGenderByLikes <- function(userid)
 {
-  result = python.get(sprintf("knnSingle('%s', %d, %d, True, True)", userid, 30, 3))
-  if (is.null(result)) result = 1
-  return(round(as.numeric(result) + 0.01))
+  return(as.numeric(python.get(sprintf("knnSingle('%s', %d, %d, True, False, '1.0')", userid, 43, 3))))
 }
 
-#predictGenderByLikes <- function(userid, dfRelations, model)
-#{
-#  cols = iggender[1:100, 1]
-#  df = data.frame(matrix(nrow=1, ncol=length(cols)))
-#  colnames(df) = cols
-#  for (i in 1:ncol(df)) 
-#  {
-#    df[,i] = as.factor(df[,i])
-#    levels(df[,i]) = c('0','1')
-#  }
-#  df = rbind(df, as.numeric(cols %in% dfRelations[dfRelations$userid == userid, 'like_id']))
-#  df = df[-1,]
-#  return(predict(model, df))
-#}
-
-predictPersonalityByLikes <- function(userid, dfRelations, model, featRange)
+predictAgeByLikes <- function(userid)
 {
-  cols = tops[featRange]
-  df = data.frame(matrix(nrow=1, ncol=length(cols)))
-  colnames(df) = cols
-  for (i in 1:ncol(df)) 
-  {
-    df[,i] = as.integer(df[,i])
-  }
-  df = rbind(df, as.integer(cols %in% dfRelations[dfRelations$userid == userid, 'like_id']))
-  df = df[-1,]
-  return(predict(model, df))
+  result = python.get(sprintf("knnSingle('%s', %d, %d, True, True, '4')", userid, 19, 2))
+  if (result == '4') return('xx-24')
+  if (result == '3') return('25-34')
+  if (result == '2') return('35-49')
+  return('50-xx')
 }
-
 
 convertAge <-function(column)
 {
