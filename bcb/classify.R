8f60ec9ee3cef98b08b6c9b15fb9459a42400d38
Darren <darrenhonsoftware@gmail.com>
2016-05-26 15:31:59 -0700

    Fix classify

--- bcb/classify.R
+++ bcb/classify.R
@@ -4,26 +4,28 @@ library(e1071) #naivebayes
 library(nnet) #multinom and ann
 library(class) #knn
 
-ann <- function(df, target, size)
+ann <- function(df, target, size, bin=F)
 {
+  message('ann ', target)
   y = class.ind(df[,target])
   train = function(data, target) nnet(data[,!names(data) %in% target], class.ind(data[,target]), size=size, softmax=T, maxit=200)
   pdt = function(model, data) predict(model, data[,!names(data) %in% target], type='class')
-  kxvalid(5, df, target, train, pdt)
+  kxvalid(5, df, target, train, pdt, bin)
 }
 
-Knn <- function(df, target, k)
+Knn <- function(df, target, k, bin=F)
 {
+  message('knn ', target)
   train = function(data, target) data
   pdt = function(model, data) knn(train = model[,!names(model) %in% target], test = data[,!names(data) %in% target], cl = model[,target], k = k) 
-  kxvalid(5, df, target, train, pdt)
+  kxvalid(5, df, target, train, pdt, bin)
 }
 
 multinomlogistic <- function(df, target)
 {
   message('multinomlogistic ', target)
   train = function(data, target) multinom(as.formula(paste(target,'~.',sep='')), data)
-  kxvalid(5, df, target, train, predict)
+  kxvalid(5, df, target, train, predict, F)
 }
 
 logistic <- function(df, target)
@@ -31,43 +33,62 @@ logistic <- function(df, target)
   message('logistic ', target)
   train = function(data, target) glm(as.formula(paste(target,'~.',sep='')), data, family=binomial())
   pdt = function(model, data) round(predict(model, data, type='response'))
-  kxvalid(5, df, target, train, pdt)
+  kxvalid(5, df, target, train, pdt, T)
 }
 
-naivebayes <- function(df, target)
+naivebayes <- function(df, target, bin=F)
 {
   message('naivebayes ', target)
   train = function(data, target) naiveBayes(as.formula(paste(target,'~.',sep='')), data)
-  kxvalid(5, df, target, train, predict)
+  kxvalid(5, df, target, train, predict, bin)
 }
 
-adaboost <- function(df, target)
+adaboost <- function(df, target, bin=F)
 {
   message('adaboost ', target)
   message('acc ', 1 - boosting.cv(as.formula(paste(target,'~.',sep='')), df, v = 5)$error)
 }
 
-decisiontree <- function(df, target)
+decisiontree <- function(df, target, bin=F)
 {
   message('decisiontree ', target)
   train = function(data, target) rpart(as.formula(paste(target,'~.',sep='')), data, method='class')
   pdt = function(model, data) predict(model, data, type='class')
-  kxvalid(5, df, target, train, pdt)
+  kxvalid(5, df, target, train, pdt, bin)
 }
 
 # train(data, target)
 # pdt(model, data)
-kxvalid <- function(k, df, target, train, pdt)
+kxvalid <- function(k, df, target, train, pdt, bin)
 {
-  correct = 0
+  tt = 0
+  ttp = NA
+  ttn = NA
+  tfp = NA
+  tfn = NA
   for (i in 1:k)
   {
-    b = (i - 1) * nrow(df) / k
+    b = ((i - 1) * nrow(df) / k) + 1
     e = i * nrow(df) / k
-    result = pdt(train(df[-(b:e),], target), df[b:e,]) 
-    correct = correct + table(result == df[b:e, target])['TRUE']
-    message(k, '-fold ', i, ' round acc ', correct / (i * nrow(df) / k))
+    res = pdt(train(df[-(b:e),], target), df[b:e,]) 
+    ans = df[b:e,target]
+    t = sum(res == ans)
+    message(k, '-fold ', i, ' round acc ', t / (floor(e) - floor(b) + 1))
+    tt = tt + t
+    if (bin)
+    {
+      tp = sum(res == ans & res == 1)
+      tn = sum(res == ans & res == 0)
+      fp = sum(res != ans & res == 1)
+      fn = sum(res != ans & res == 0)
+      ttp = if (is.na(ttp)) tp else ttp + tp
+      ttn = if (is.na(ttn)) tn else ttn + tn
+      tfp = if (is.na(tfp)) fp else tfp + fp
+      tfn = if (is.na(tfn)) fn else tfn + fn
+      message(k, '-fold ', i, ' round pre ', tp / (tp + fp), ' sen ', tp / (tp + fn), ' spe ', tn/(tn+fp))
+    }
   }
   message('acc ', correct / nrow(df))
   message('majority ', max(table(df[,target]))/nrow(df))
+  if (bin) message('pre ', ttp / (ttp + tfp), ' sen ', ttp / (ttp + tfn), ' spe ', ttn/(ttn+tfp))
 }
--- bcb/classifyAll.R
+++ bcb/classifyAll.R
@@ -2,7 +2,10 @@ source('classify.R')
 library(data.table)
 
 df = fread(commandArgs(trailingOnly = TRUE)[1], data.table=F)
-df = df[,!names(df) %in% c('diag_p','proc_p','admitDT','dischargeDT','PID','diag_p_ccs','proc_p_ccs')]
+df = df[,!names(df) %in% c('diag_p', 'proc_p', 'admitDT', 'dischargeDT', 'PID', 'diag_p_ccs', 'proc_p_ccs', 'nextCost', 'nextLOS')]
+
+facCol = c('type_care','gender','srcsite','srcroute','schedule','oshpd_destination','race_grp','msdrg_severity_ill','thirtyday','sameday','LOS_b','nextLOS_b','cost_b','nextCost_b')
+for (col in facCol) df[,col] = as.factor(df[,col])
 
 # thrityday
 df2 = df[,!names(df) %in% c('nextCost_b','nextLOS_b')]
