0cbe0c514a84502cc774679b35ed8f5c2a360228
darrenhon <darrenhonsoftware@gmail.com>
2016-08-17 07:47:15 +0000

    runModel skips patients less than 4 records

--- thesis/runModels.r
+++ thesis/runModels.r
@@ -1,16 +1,22 @@
 path = commandArgs(trailingOnly = TRUE)[1]
 pathmodels = commandArgs(trailingOnly = TRUE)[2]
+fset = commandArgs(trailingOnly = TRUE)[3]
+datarange = commandArgs(trailingOnly = TRUE)[4]
 
 library(data.table)
 library(rpart)
 library(pROC)
 
+message('fset ', fset)
 message('Loading models...')
 models = readRDS(pathmodels)
 maxlen = length(models[[1]])
 message('Done loading models')
 
-df = fread(path, data.table=F)[1:10000,]
+df = fread(path, data.table=F)
+
+# take a subset of data
+if (length(commandArgs(trailingOnly = TRUE)) >= 4) df = df[eval(parse(text=paste('c(',datarange,')',sep=''))), ]
 
 # remove unused variables
 df = df[,!names(df) %in% c('admitDT', 'dischargeDT', 'nextCost', 'nextLOS','LOS_b','cost_b', 'nextCost_b', 'nextLOS_b')]
@@ -23,6 +29,7 @@ fcom = names(df)[grepl('ch_com', names(df))]
 fcum = c('coms', 'cons', 'er6m', 'adms', 'lace')
 
 allfeats = c(fdemo, fclos, fadmin, fcom, fcum)
+if (length(commandArgs(trailingOnly = TRUE)) >= 3) allfeats = allfeats[eval(parse(text=paste('c(',fset,')',sep='')))]
 
 # turn variables into factor
 facCol = c('thirtyday', 'type_care','gender','srcsite','srcroute','schedule','oshpd_destination','race_grp','msdrg_severity_ill','sameday', 'merged', fcom)
@@ -30,13 +37,16 @@ for (col in facCol) df[,col] = as.factor(df[,col])
 
 pids = unique(df$PID)
 probs = c()
+ans = c()
 count = 1
 for (pid in pids)
 {
   if ((count %% floor(length(pids) / 100)) == 0) message('Running ', count * 100 / length(pids), '%')
   count = count + 1
   rows = df[df$PID == pid,]
-  for (i in 1:nrow(rows))
+  # skip patients less than 4 records
+  if (nrow(rows) < 4) next
+  for (i in 4:nrow(rows))
   {
     thisprobs = c()
     for (col in allfeats)
@@ -52,8 +62,10 @@ for (pid in pids)
         message('Error in column ', col, ', pid ', pid, '. Probability skipped\n', err)
       })
     }
+    if (length(thisprobs) == 0) thisprobs = c(0)
     probs = c(probs, mean(thisprobs))
+    ans = c(ans, rows[i, 'thirtyday'])
   }
 }
 
-message('auc ', auc(df[,'thirtyday'], probs)
+message('auc ', auc(ans, probs))
