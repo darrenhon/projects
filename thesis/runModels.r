5d7a1097650c99dfd6d1992a638d4af51747784c
darrenhon <darrenhonsoftware@gmail.com>
2016-09-29 09:31:09 +0000

    Fixed bugs in runModel.r. Add filter seq length, take last n sym, and specify weights.

--- thesis/runModels.r
+++ thesis/runModels.r
@@ -3,12 +3,24 @@ pathmodels = commandArgs(trailingOnly = TRUE)[2]
 lrmodel = commandArgs(trailingOnly = TRUE)[3]
 fset = commandArgs(trailingOnly = TRUE)[4]
 datarange = commandArgs(trailingOnly = TRUE)[5]
+filterseqlen = commandArgs(trailingOnly = TRUE)[6]
+takelastnsym = commandArgs(trailingOnly = TRUE)[7]
+weights = commandArgs(trailingOnly = TRUE)[8]
+
+valid = function(arg)
+{
+  return(!is.na(arg) & arg != 'NA')
+}
 
 library(data.table)
 library(rpart)
 library(pROC)
 
 message('fset ', fset)
+message('datarange ', datarange)
+message('filterseqlen ', filterseqlen)
+message('takelastnsym ', takelastnsym)
+message('weights ', weights)
 message('Loading models...')
 lr = readRDS(lrmodel)
 models = readRDS(pathmodels)
@@ -18,7 +30,7 @@ message('Done loading models')
 df = fread(path, data.table=F)
 
 # take a subset of data
-if (!is.na(datarange)) df = df[eval(parse(text=paste('c(',datarange,')',sep=''))), ]
+if (valid(datarange)) df = df[eval(parse(text=paste('c(',datarange,')',sep=''))), ]
 
 # remove unused variables
 df = df[,!names(df) %in% c('admitDT', 'dischargeDT', 'nextCost', 'nextLOS','LOS_b','cost_b', 'nextCost_b', 'nextLOS_b')]
@@ -42,11 +54,15 @@ probs = as.list(rep(NA, length(allfeats) + 1))
 names(probs) = c(allfeats, 'lr')
 ans = c()
 count = 1
+maj = table(df$thirtyday)['1'] / nrow(df)
 for (pid in pids)
 {
   if ((count %% floor(length(pids) / 100)) == 0) message('Running ', count * 100 / length(pids), '%')
   count = count + 1
   rows = df[df$PID == pid,]
+  # filter sequence length
+  if (valid(filterseqlen) & nrow(rows) != filterseqlen)
+    next
   # do it for all rows
   #for (i in 1:nrow(rows))
   # skip patients less than 4 records
@@ -59,13 +75,16 @@ for (pid in pids)
     for (col in allfeats)
     {
       start = max(1, i - maxlen + 1)
+      vals = rows[start:i, col]
+      if (valid(takelastnsym))
+        vals = vals[1:min(as.numeric(takelastnsym), length(vals))]
       vals = as.list(rows[start:i, col])
       names(vals) = sapply(1:length(vals), function(x) paste('X', x, sep=''))
       tryCatch({
         probs[[col]] = c(probs[[col]], predict(models[[col]][[length(vals)]], vals)[2])
       }, error = function(err)
       {
-        probs[[col]] <<- c(probs[[col]], 0.3434)
+        probs[[col]] <<- c(probs[[col]], maj)
         message('Error in column ', col, ', pid ', pid, '. Use majority prob.\n', err)
       })
     }
@@ -88,13 +107,21 @@ avg = Reduce('+', probs) / length(probs)
 message('auc mean all features probs ', auc(ans, avg))
 message('auc lr only ', auc(ans, probs[['lr']]))
 
-message('optimizing auc with lr')
-res = optim(c(rep(0, length(allfeats)), 1), weightedAvgAucNeg, lower=0, upper=1, method='L-BFGS-B', allprobs=probs, ans=ans)
-message('auc optimized ', -res$value)
-message('weights ', paste(res$par/sum(res$par), collapse=' ,'))
+if (valid(weights))
+{
+  probs = probs[names(probs) != 'lr']
+  weights = eval(parse(text=paste('c(', weights,')',sep='')))
+  message('auc without lr from provided weights ', -weightedAvgAucNeg(weights, probs, ans))
+} else
+{
+  message('optimizing auc with lr')
+  res = optim(c(rep(0, length(allfeats)), 1), weightedAvgAucNeg, lower=0, upper=1, method='L-BFGS-B', allprobs=probs, ans=ans)
+  message('auc optimized ', -res$value)
+  message('weights ', paste(res$par/sum(res$par), collapse=' ,'))
 
-probs = probs[names(probs) != 'lr']
-message('optimizing auc without lr')
-res = optim(rep(1, length(allfeats)), weightedAvgAucNeg, lower=0, upper=1, method='L-BFGS-B', allprobs=probs, ans=ans)
-message('auc optimized ', -res$value)
-message('weights ', paste(res$par/sum(res$par), collapse=' ,'))
+  probs = probs[names(probs) != 'lr']
+  message('optimizing auc without lr')
+  res = optim(rep(1, length(allfeats)), weightedAvgAucNeg, lower=0, upper=1, method='L-BFGS-B', allprobs=probs, ans=ans)
+  message('auc optimized ', -res$value)
+  message('weights ', paste(res$par/sum(res$par), collapse=' ,'))
+}
