de373283c0dae35953b19b866b7cb2182195f257
darrenhon <darrenhonsoftware@gmail.com>
2017-01-06 01:48:18 -0800

    Fix socket blocking issue

--- xbox/xbox.py
+++ xbox/xbox.py
@@ -2,7 +2,6 @@ import sys, socket, select, time
 
 def wake(address, liveid):
   with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.setblocking(0)
     s.bind(("", 0))
     s.connect((address, 5050))
     payload = bytes([0, len(liveid)]) + liveid.encode('utf-8') + bytes([0])
@@ -11,7 +10,6 @@ def wake(address, liveid):
 
 def ping(address):
   with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.setblocking(0)
     s.bind(("", 0))
     s.connect((address, 5050))
     s.send(bytearray.fromhex("dd00000a000000000000000400000002"))
